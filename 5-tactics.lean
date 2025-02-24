a763710378ecd70ebccae8b80d985482e5014165
joongwon <freleefty@gmail.com>
2025-02-24 21:20:25 +0900

    end

--- 5-tactics.lean
+++ 5-tactics.lean
@@ -1,1 +1,62 @@
 -- 2025-02-24
+
+theorem test (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  · exact hp
+  · apply And.intro
+    · exact hq
+    · exact hp
+
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  . apply Or.inr
+    assumption
+  . apply Or.inl
+    assumption
+
+example : ∃ x, x + 2 = 8 := by
+  let a : Nat := 3 * 2
+  exists a
+
+/-
+-- fails: maximum recursion depth has been reached
+example : ∃ x, x + 2 = 8 := by
+  repeat (try assumption)
+  -/
+
+open List
+
+example (xs : List Nat)
+        : reverse (xs ++ [1, 2, 3]) = [3, 2, 1] ++ reverse xs := by
+  simp
+
+example (xs ys : List α)
+        : length (reverse (xs ++ ys)) = length xs + length ys := by
+  -- simp [Nat.add_comm]
+  simp
+  apply Nat.add_comm
+
+example (u w x y z : Nat) (h₁ : x = y + z) (h₂ : w = u + x)
+        : w = z + y + u := by
+  simp [*, Nat.add_assoc, Nat.add_comm, Nat.add_left_comm]
+
+
+def f (x y z : Nat) : Nat :=
+  match x, y, z with
+  | 5, _, _ => y
+  | _, 5, _ => y
+  | _, _, 5 => y
+  | _, _, _ => 1
+
+example (x y z : Nat) : x ≠ 5 → y ≠ 5 → z ≠ 5 → z = w → f x y w = 1 := by
+  intros
+  simp [f]
+  split
+  . contradiction
+  . contradiction
+  . contradiction
+  . rfl
+
+-- end; tomorrow todo: solve exercises of chapter 5
